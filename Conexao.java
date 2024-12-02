a34f7a29b536e7e9cba1f2c2b880650f49e2d75b
Francisco <pedrofran.lopes2004@gmail.com>
2024-12-02 20:02:22 -0300

    Update Conexao.java

--- Conexao.java
+++ Conexao.java
@@ -1,24 +1,29 @@
 package model;
-	import java.sql.Connection;
-	import java.sql.DriverManager;
-	import java.sql.SQLException;
+
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
 public class Conexao {
-	
 	public static Connection getConn() {
 		Connection conn = null;
 		
 		try {
-			Class.forName("com.mysql://localhost:3306/cadastro","root","");
-			conn = DriverManager.getConnection("");
-		}catch(ClassNotFoundException e) {
-			System.out.println("Erro ao localizar o driver");
-		}catch(SQLException e) {
-			System.out.println("Erro na conexão");
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/usuario" , "root" , "");
+		} catch(ClassNotFoundException e) {
+			System.out.println("Erro ao localizar o drive "+e.getMessage());
+		} catch(SQLException e) {
+			System.out.println("Erro ao localizar o servidor"+e.getMessage());
 		}
 		
 		return conn;
-		
 	}
-	
-	
 }
+
+	
+	
+
