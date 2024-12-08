486db26794580169090fce1be0700550079af718
Francisco <pedrofran.lopes2004@gmail.com>
2024-12-08 11:26:54 -0300

    Conexao.java

---
+++ model/Conexao.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.sql.Connection; 
+
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+	public static Connection getConn() {
+		Connection conn = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/usuario", "root","");
+		}catch(ClassNotFoundException e) {
+			System.out.println("Erro ao localizar o driver");
+		}catch (SQLException e) {
+			System.out.println("Erro na conexão");
+		}
+		return conn;
+	}
+
+}
